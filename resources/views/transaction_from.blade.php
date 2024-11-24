43c3585acb6f26fc32ba7a505c9aee4e43fee7a8
Saeid Khaleghi <saeid.khaleghi@gmail.com>
2024-11-24 14:04:49 +0330

    Add foreign currency code and amount to Transaction's from_my_client

--- resources/views/transaction_from.blade.php
+++ resources/views/transaction_from.blade.php
@@ -1,5 +1,9 @@
 <t_from_my_client>
     <from_funds_code>{{ $from->from_fund_code }}</from_funds_code>
+    <from_foreign_currency>
+        <foreign_currency_code>{{ $from->from_foreign_currency_code }}</foreign_currency_code>
+        <foreign_amount>{{ $from->from_foreign_amount }}</foreign_amount>
+    </from_foreign_currency>
     @each('reporter::from_account', $from->from_accounts, 'from_account')
     <from_country>{{ $from->from_country }}</from_country>
 </t_from_my_client>
--- src/Tags/TransactionFrom.php
+++ src/Tags/TransactionFrom.php
@@ -5,19 +5,23 @@ namespace SaeidKhaleghi\PoliceReporter\Tags;
 class TransactionFrom extends Tag
 {
     public string $from_fund_code;
+    public string $from_foreign_currency_code;
+    public string $from_foreign_amount;
     public string $from_country;
 
     /** @var FromAccount[] */
     public array $from_accounts = [];
 
-    public static function create(string $from_fund_code, string $from_country): static
+    public static function create(string $from_fund_code, string $from_foreign_currency_code, string $from_foreign_amount, string $from_country): static
     {
-        return new static($from_fund_code, $from_country);
+        return new static($from_fund_code, $from_foreign_currency_code, $from_foreign_amount, $from_country);
     }
 
-    public function __construct(string $from_fund_code, string $from_country)
+    public function __construct(string $from_fund_code, string $from_foreign_currency_code, string $from_foreign_amount, string $from_country)
     {
         $this->from_fund_code = $from_fund_code;
+        $this->from_foreign_currency_code = $from_foreign_currency_code;
+        $this->from_foreign_amount = $from_foreign_amount;
         $this->from_country = $from_country;
     }
 
